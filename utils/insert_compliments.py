c8fcefe7e804772401fc95c3b46db050656ff83e
kritohanzo <kritohanzo0@gmail.com>
2024-02-13 12:36:01 +0500

    add compliments insert

---
+++ utils/insert_compliments.py
@@ -0,0 +1,11 @@
+from utils.db import Database as db
+from utils.models import Compliment
+
+with open('compliments.txt') as file:
+    compliments = (string.strip() for string in file)
+
+with db.session() as session:
+    for compliment in compliments:
+        compliment_obj = Compliment(text=compliment)
+        session.add()
+    session.commit()
