147b0fb3109e59b0d85dc9ecbd3dc7ed0bd076fa
kritohanzo <kritohanzo0@gmail.com>
2024-02-13 13:54:42 +0500

    updat db url

--- utils/db.py
+++ utils/db.py
@@ -26,7 +26,7 @@ class Database:
             # Base.metadata.create_all(bind=engine)
             alembic_cfg  = Config()
             alembic_cfg.set_main_option('script_location', "migration/")
-            alembic_cfg.set_main_option('sqlalchemy.url', 'sqlite:///db.sqlite3')
+            alembic_cfg.set_main_option('sqlalchemy.url', "postgresql+psycopg2://valentine_user:valentine_password@db/valentine_db")
             command.upgrade(alembic_cfg, 'head')
             with cls.session() as session:
                 insert_compliments(session)
